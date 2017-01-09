1c61acbbb111389a6c56b3642399140e2d3074f2
harrypower <philipkingsmith@gmail.com>
2017-01-09 01:09:01 -0700

    new rn171 stuff

--- setup.sh
+++ setup.sh
@@ -3,11 +3,14 @@
 sudo rm /var/www/html/index.html
 sudo rm /usr/lib/cgi-bin/Gforthmail.cgi
 sudo rm /usr/lib/cgi-bin/Gforthmailget.cgi
+sudo rm /usr/lib/cgi-bin/RN171-cgi-get.cgi
 
 sudo cp index.html /var/www/html/index.html
 sudo cp Gforthmail.cgi /usr/lib/cgi-bin/Gforthmail.cgi
 sudo cp Gforthmailget.cgi /usr/lib/cgi-bin/Gforthmailget.cgi
+sudo cp RN171-cgi-get.cgi /usr/lib/cgi-bin/RN171-cgi-get.cgi
+sudo touch /run/cgitest.tmp
 
 sudo chmod 755 /usr/lib/cgi-bin/Gforthmail.cgi
 sudo chmod 755 /usr/lib/cgi-bin/Gforthmailget.cgi
-
+suod chmod 755 /usr/lib/cgi-bin/RN171-cgi-get.cgi

f5230d75ed6d1be6fac7a7be2f698ba39ba450cb
harrypower <philipkingsmith@gmail.com>
2017-01-12 16:29:18 -0700

    clean up files

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ sudo rm /usr/lib/cgi-bin/Gforthmail.cgi
 sudo rm /usr/lib/cgi-bin/Gforthmailget.cgi
 sudo rm /usr/lib/cgi-bin/RN171-cgi-get.cgi
 sudo rm /usr/lib/cgi-bin/last-RN171-data.cgi
+sudo rm /run/cgitest.tmp
+sudo rm /run/cgimail.tmp
 
 sudo cp index.html /var/www/html/index.html
 sudo cp Gforthmail.cgi /usr/lib/cgi-bin/Gforthmail.cgi
